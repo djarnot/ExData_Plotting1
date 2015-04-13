56d95785c05fadbf790fd5f55279ac69958b58d7
DataDJ <djarnot@users.noreply.github.com>
2015-04-12 14:25:07 -1000

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,6 +1,21 @@
 
 
+## get data
+household_power_consumption <- read.csv("household_power_consumption.txt", sep=";", na.strings="?")
+household_power_consumption$Date <- as.Date(household_power_consumption$Date, format="%d/%m/%Y")
 
+## subset the data
+data <- subset(household_power_consumption, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(household_power_consumption)
 
+## Convert dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
 
-household_power_consumption <- read.csv("~/Desktop/Exp_Data_R/household_power_consumption.txt", sep=";", na.strings="?")
+## Plot 1
+hist(data$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+## Saving to file
+dev.copy(png, file="plot1.png", height=400, width=400)
+dev.off()
